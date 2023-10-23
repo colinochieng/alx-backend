c92a2a65d3c4f5873dbfa1e4ad8c49ae38508701
colinochieng <colinokumu89@gmail.com>
2023-10-23 19:38:15 +0300

    Pagination

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+"""
+Main file
+"""
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    desc: function to computes page indexes
+    Args:
+        page(int): page number
+        page_size(int): size of page
+    return:  a tuple of size two containing a start index
+        and an end index corresponding to the range of
+        indexes to return in a list for those particular
+        pagination parameters
+    '''
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        desc: method to return info of a page
+        return: a list of data list or an empty list
+        '''
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+
+        first_idx, end_idx = index_range(page, page_size)
+
+        try:
+            return self.dataset()[first_idx: end_idx]
+        except IndexError:
+            return []
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, int]:
+        '''
+        desc: function for Hypermedia pagination
+        Return: dictionary containing the following key-value pairs:
+            - page_size: the length of the returned dataset page
+            - page: the current page number
+            - data: the dataset page
+            - next_page: number of the next page, None if no next page
+            - prev_page: number of the previous page, None if no previous page
+            - total_pages: the total number of pages in the dataset as an integer
+        '''
+        total_pages = (len(self.dataset()) + page_size - 1) // page_size
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        data = self.get_page(page, page_size)
+
+        return {
+            'page_size': data.__len__(),
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
